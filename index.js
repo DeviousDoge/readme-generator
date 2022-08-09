5e96f7490fbdc3e27b2b56912d92d96b2e739f6a
Sean Dillon <pdillon0@gmail.com>
2022-08-09 19:19:54 -0400

    index.js functions defined, prompt works.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const generate = require('./utils/generateMarkdown.js');
 const path = require('path');
 // TODO: Include packages needed for this application
-inquirer
+
   // TODO: Create an array of questions for user input
  const questions = [
     {
@@ -55,10 +55,18 @@ inquirer
  ];
  
   // TODO: Create a function to write README file
- function writeToFile(fileName, data) {}
+ function writeToFile(fileName, data) {
+    fs.writeFileSync(path.join (process.cwd(), "Readme.md"), generate(response));
+ }
 
   // TODO: Create a function to initialize app
- function init() {}
+ function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+      return writeToFile();
+    });
+ }
 
   // Function call to initialize app
 init();
