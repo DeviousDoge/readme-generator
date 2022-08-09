72f1a02a6c659bdbf22bbfa000b8c1c56bba340f
Sean Dillon <pdillon0@gmail.com>
2022-08-09 18:32:30 -0400

    packages and structure

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generate = require('./utils/generateMarkdown.js');
+const path = require('path');
 // TODO: Include packages needed for this application
+inquirer
+  // TODO: Create an array of questions for user input
+ const questions = [
+    {
+      type: 
+      message:
+      name:
+         },
+    {
+       type:
+       message:
+       name:
+         },
+    {
+      type:
+      message:
+      name:
+         },    
+    {
+       type:
+       message:
+       name:
+         },    
+    {
+      type:
+      message:
+      name:
+         }, 
+    {
+       type:
+       message:
+       name:
+         },
+    {
+      type:
+      message:
+      name:
+         },
+    {
+       type:
+       message:
+       name:
+         },
+    {
+      type:
+      message:
+      name:
+         },
+ ];
 
-// TODO: Create an array of questions for user input
-const questions = [];
+  // TODO: Create a function to write README file
+ function writeToFile(fileName, data) {}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+  // TODO: Create a function to initialize app
+ function init() {}
 
 // Function call to initialize app
 init();
